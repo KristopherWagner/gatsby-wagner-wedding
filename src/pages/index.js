7d8688e1bdb55a86d137db1aba52fcfadec377fd
KristopherWagner <kmwagner91@gmail.com>
2022-05-14 12:38:29 -0400

    Add an image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react';
 
 export default function IndexPage() {
   return (
-    <Layout>
-      <h1>Welcome to our wedding site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </Layout>
-  )
+    <>
+      <div style={{ height: '100vh', width: '100vw' }}>
+        <StaticImage
+          alt="Shannon and Kristopher at the Starlight Lounge on the Halcyon"
+          src="../images/starlight_lounge.jpg"
+        />
+      </div>
+      <Layout>
+        <h1>Welcome to our wedding site!</h1>
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
+    </>
+  );
 }
