e1564600fb4fd447961d5f290915892164a0921c
KristopherWagner <kmwagner91@gmail.com>
2022-05-14 14:11:03 -0400

    Use a parallax scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-
-import { StaticImage } from 'gatsby-plugin-image';
+import { withPrefix } from 'gatsby';
 import Layout from '../components/layout';
 
 export default function IndexPage() {
   return (
     <>
-      <div style={{ height: '100vh', width: '100vw' }}>
-        <StaticImage
-          alt="Shannon and Kristopher at the Starlight Lounge on the Halcyon"
-          src="../images/starlight_lounge.jpg"
-        />
-      </div>
+      <div
+        style={{
+          backgroundImage: `url(${withPrefix('/images/starlight_lounge.jpg')})`,
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+
+          minHeight: '100vh',
+        }}
+      />
       <Layout>
         <h1>Welcome to our wedding site!</h1>
         <p>I&lsquo;m making this by following the Gatsby Tutorial.</p>
