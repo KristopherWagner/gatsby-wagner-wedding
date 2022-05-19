e4d73e118209fea9b839a0f8cce0d6da0f136e42
KristopherWagner <kmwagner91@gmail.com>
2022-05-19 12:58:25 -0400

    #11 Fix layout shift

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -2,13 +2,25 @@ import React from 'react';
 import propTypes from 'prop-types';
 import styled from 'styled-components';
 
-const ParallaxDiv = styled.div`
+const Wrapper = styled.div`
+  position: relative;
+  height: calc(1536 / 2309 * 100vw);
+`;
+
+const Image = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    max-width: 100%;
+    height: auto;
+
     aspect-ratio: 2309/1536;
     background-attachment: scroll;
     background-image: ${(props) => `url(${props.url})`};
     background-position-y: 0;
     background-repeat: no-repeat;
     background-size: contain;
+    min-width: 100vw;
 
     @media (min-width: 1080px) {
       background-attachment: fixed;
@@ -19,7 +31,11 @@ const ParallaxDiv = styled.div`
 `;
 
 export default function Parallax({ url }) {
-  return <ParallaxDiv url={url} />;
+  return (
+    <Wrapper>
+      <Image url={url} />
+    </Wrapper>
+  );
 }
 
 Parallax.propTypes = {
