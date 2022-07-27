f8d96241cc7e86cc6a5a9438f372485ef9a7ca82
KristopherWagner <kmwagner91@gmail.com>
2022-07-27 16:23:08 -0400

    #2 style the travel section better

--- src/components/Travel/Grid.js
+++ src/components/Travel/Grid.js
@@ -4,14 +4,21 @@ import styled from 'styled-components';
 
 const StyledGrid = styled.div`
   display: grid;
-  grid-template-columns: 33% 33% 33%;
+  grid-template-columns: ${
+  (props) => (props.columns === 3 ? '33% 33% 33%' : '50% 50%')
+};
 `;
 
 export default function Grid(props) {
-  const { children } = props;
-  return <StyledGrid>{children}</StyledGrid>;
+  const { children, columns } = props;
+  return <StyledGrid columns={columns}>{children}</StyledGrid>;
 }
 
 Grid.propTypes = {
   children: PropTypes.node.isRequired,
+  columns: PropTypes.oneOf([2, 3]),
+};
+
+Grid.defaultProps = {
+  columns: 3,
 };
