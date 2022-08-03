009b24d779c3fcd1ff6d37a0993b51199d405e12
KristopherWagner <kmwagner91@gmail.com>
2022-08-03 16:39:19 -0400

    #3 Use a Javascript solution

--- src/components/About/FlipCard.js
+++ src/components/About/FlipCard.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import './FlipCard.css';
+const Root = styled.div`
+  align-self: center;
+  background-color: transparent;
+  display: inline-block;
+  justify-self: center;
+  margin-top: 16px;
+  perspective: 1000px;
+
+  height: 300px;
+  width: 300px;
+`;
+
+const Container = styled.div`
+  box-shadow: 0 3px 6px black;
+  position: relative;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+
+  height: 100%;
+  width: 100%;
+
+  &:hover {
+    transform: rotateY(180deg);
+  }
+`;
+
+const Front = styled.div`
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  position: absolute;
+
+  height: 100%;
+  width: 100%;
+`;
+
+const Back = styled(Front)`
+  background-color: #B9E4C9;
+  transform: rotateY(180deg);
+`;
+
+const Memory = styled.p`
+  padding: 4px;
+`;
 
 export default function FlipCard(props) {
   const { image, memory, name } = props;
 
   return (
-    <div className="flip-card">
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-      <div className="flip-card-inner" tabIndex={0}>
-        <div className="flip-card-front">
+    <Root>
+      <Container>
+        <Front>
           {image}
-        </div>
-        <div className="flip-card-back">
+        </Front>
+        <Back>
           <h3>{name}</h3>
-          <p className="memory">{memory}</p>
-        </div>
-      </div>
-    </div>
+          <Memory>{memory}</Memory>
+        </Back>
+      </Container>
+    </Root>
   );
 }
 
