f938148f8492b2f6a340af136f174ff980e31e09
KristopherWagner <kmwagner91@gmail.com>
2022-08-03 14:42:43 -0400

    #3 Use grid layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,6 +18,27 @@ const Jesse = <StaticImage alt="Jesse" src="../../images/Jesse.webp" {...SHARED_
 const Jordan = <StaticImage alt="Jordan" src="../../images/Jordan.webp" {...SHARED_PROPS} />;
 const Kelly = <StaticImage alt="Kelly" src="../../images/Kelly.webp" {...SHARED_PROPS} />;
 
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+
+  @media (min-width: 616px) {
+    grid-template-columns: 50% 50%;
+  }
+
+  @media (min-width: 924px) {
+    grid-template-columns: 33% 34% 33%;
+  }
+ 
+  @media (min-width: 1232px) {
+    grid-template-columns: 25% 25% 25% 25%;
+  }
+
+  @media (min-width: 1540px) {
+    grid-template-columns: 20% 20% 20% 20% 20%;
+  }
+`;
+
 const Root = styled.div`
   background-color: ${BACKGROUND_LIGHT};
   padding: 24px 0px;
@@ -33,31 +54,33 @@ export default function About() {
     <Root id="about">
       <Title>The Wedding Party</Title>
       <p>Meet the attendants for our wedding and learn their favorite Disney memory</p>
-      <FlipCard
-        image={Casper}
-        memory="Spending the night in Cinderella Castle and being a part of the parade during the year of a million dreams"
-        name="Casper Wagner"
-      />
-      <FlipCard
-        image={Ella}
-        memory="Seeing Cinderella Castle from the ferry boat for the first time."
-        name="Ella Melzer"
-      />
-      <FlipCard
-        image={Jesse}
-        memory="Riding the Kilimanjaro Safari with my wife."
-        name="Jesse Sharkawy"
-      />
-      <FlipCard
-        image={Jordan}
-        memory="Spending an entire day meeting characters and collecting signatures for my grad cap."
-        name="Jordan Barrett"
-      />
-      <FlipCard
-        image={Kelly}
-        memory="Feeling like I'm flying on the Avatar Flight of Passage ride in Pandora."
-        name="Kelly Swanson-Obando"
-      />
+      <CardContainer>
+        <FlipCard
+          image={Casper}
+          memory="Spending the night in Cinderella Castle and being a part of the parade during the year of a million dreams"
+          name="Casper Wagner"
+        />
+        <FlipCard
+          image={Ella}
+          memory="Seeing Cinderella Castle from the ferry boat for the first time."
+          name="Ella Melzer"
+        />
+        <FlipCard
+          image={Jesse}
+          memory="Riding the Kilimanjaro Safari with my wife."
+          name="Jesse Sharkawy"
+        />
+        <FlipCard
+          image={Jordan}
+          memory="Spending an entire day meeting characters and collecting signatures for my grad cap."
+          name="Jordan Barrett"
+        />
+        <FlipCard
+          image={Kelly}
+          memory="Feeling like I'm flying on the Avatar Flight of Passage ride in Pandora."
+          name="Kelly Swanson-Obando"
+        />
+      </CardContainer>
     </Root>
   );
 }
