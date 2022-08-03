2a6b3faac1e4e7dc43e49ad229475b0f83c91235
KristopherWagner <kmwagner91@gmail.com>
2022-08-03 15:04:25 -0400

    Add additional colors

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,6 +1,8 @@
+export const BRIDESMAID = '#FEEAE6';
+export const FRINGY_FLOWER = '#B9E4C9';
 const MAUVELOUS = '#F48FB1';
-export const NORWAY = '#9DB89F';
 const PARSELY = '#1B5E20';
+export const STROMBOLI = '#356859';
 const WILD_SAND = '#F5F5F5';
 
 export const PRIMARY = PARSELY;
