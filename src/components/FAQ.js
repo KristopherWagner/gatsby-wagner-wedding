7e14ddca3afb0eb793fa6e67b7da3dc207ce3ce3
KristopherWagner <kmwagner91@gmail.com>
2022-11-10 11:59:53 -0500

    #5 Add hotel question

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -22,6 +22,27 @@ export default function FAQ() {
         {' '}
         or a similar service. We appreciate your understanding.
       </p>
+      <h5><em>Do I need to stay at a Disney-owned hotel?</em></h5>
+      <p className="body1">
+        {'No, but there are '}
+        <Link
+          href="https://disneyworld.disney.go.com/resort-hotels-benefits/"
+          text="benefits of staying at a Disney Resort Hotel"
+        />
+        {' including '}
+        <Link
+          href="https://disneyworld.disney.go.com/special-offers/early-2023-package-offer/"
+          text="discounts on park tickets"
+        />
+        . In the &quot;Accommodations&quot; section we recommend staying at a Disney hotel.
+        If you read that section and need more help, feel free to
+        {' '}
+        <Link
+          href="mailto:kmwagner91@gmail.com"
+          text="email the groom"
+        />
+        {' for assistance.'}
+      </p>
     </>
   );
 }
