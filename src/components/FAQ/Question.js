049763ab7f5441cc2e23209e6bf2c2947b1554b7
KristopherWagner <kmwagner91@gmail.com>
2022-11-11 15:14:02 -0500

    Fix accesibility issue
    Fixes "Heading elements are not in a sequentially-descending order"
    see https://dequeuniversity.com/rules/axe/4.4/heading-order

--- src/components/FAQ/Question.js
+++ src/components/FAQ/Question.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// styling an h4 to look like an h5 for accessibility reasons
+const Heading = styled.h4`
+  font-size: 24px;
+  font-weight: 400;
+  letter-spacing: 0;
+  line-height: 1.3;
+  margin: calc(2* 1.3 * 16px) auto calc(1.3 * 16px);
+
+  @media (min-width: 1080px) {
+    font-size: 1.5rem;
+    margin: calc(2* 1.3 * 1rem) auto calc(1.3 * 1rem);
+  }
+`;
 
 export default function Question({ children }) {
   return (
-    <h5><em>{children}</em></h5>
+    <Heading><em>{children}</em></Heading>
   );
 }
 
