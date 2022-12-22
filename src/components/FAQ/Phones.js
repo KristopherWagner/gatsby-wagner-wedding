770f03e71458a8989e9a2ae1c3e87e20d8302b2e
KristopherWagner <kmwagner91@gmail.com>
2022-12-22 12:37:14 -0500

    Update phone FAQ
    Used a modified Chat GPT answer

--- src/components/FAQ/Phones.js
+++ src/components/FAQ/Phones.js
@@ -9,8 +9,14 @@ export default function Phones() {
         Can I use my phone to take pictures during the ceremony?
       </Question>
       <p className="body1">
-        No, we have chosen to have an unplugged ceremony and have hired professionals to photograph,
-        record, and live stream our ceremony. Please respect our wishes by turning off your phone.
+        As we celebrate our love and commitment to each other, we ask that our guests take this
+        opportunity to be fully present and enjoy the moment with us. In order to create a truly
+        special and memorable experience for everyone, we kindly request that guests limit their
+        phone usage during the reception and turn them off during the ceremony. We understand that
+        it can be tempting to document every moment, so we have hired professionals to photograph,
+        record and live stream our events; we hope that you will take this opportunity to be fully
+        present and fully engaged in the celebration. Thank you for helping to create a meaningful
+        and special day for us all.
       </p>
     </>
   );
