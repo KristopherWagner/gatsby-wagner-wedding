b99f826621cfcc30e7398b84cf1e138bc6ff7d0e
KristopherWagner <kmwagner91@gmail.com>
2022-12-22 12:33:52 -0500

    Add charities and carbon offsets to the registry FAQ #56
    Mostly written by Chat GPT

--- src/components/FAQ/Registry.js
+++ src/components/FAQ/Registry.js
@@ -3,6 +3,42 @@ import React from 'react';
 import Link from '../Link';
 import Question from './Question';
 
+function BatCon() {
+  return (
+    <Link
+      href="https://action.batcon.org/site/Donation2?df_id=1500&mfc_pref=T&1500.donation=form1"
+      text="Bat Conservation International"
+    />
+  );
+}
+
+function Lubee() {
+  return (
+    <Link
+      href="https://www.lubee.org/donate"
+      text="Lubee Bat Conservancy"
+    />
+  );
+}
+
+function MauiNui() {
+  return (
+    <Link
+      href="https://www.mauireefs.org/"
+      text="Maui Nui Marine Resource Council"
+    />
+  );
+}
+
+function CarbonOffsets() {
+  return (
+    <Link
+      href="https://shop.8billiontrees.com/search?q=offset"
+      text="carbon offsets"
+    />
+  );
+}
+
 export default function Registry() {
   return (
     <>
@@ -21,6 +57,23 @@ export default function Registry() {
           text="email the groom"
         />
         {'  for information on Venmo, PayPal, or Zelle.'}
+      </p>
+      <p className="body1">
+        Alternatively, we would also be honored if you would consider making a donation to one of
+        our favorite charities in lieu of a traditional wedding gift.
+        {' '}
+        <BatCon />
+        {', '}
+        <MauiNui />
+        {', and '}
+        <Lubee />
+        {' '}
+        are organizations that are close to our hearts and we believe in their missions to make a
+        positive impact in the world. You could also purchase
+        {' '}
+        <CarbonOffsets />
+        {' '}
+        for your travel to help us with our goal of having a carbon neutral wedding.
       </p>
     </>
   );
