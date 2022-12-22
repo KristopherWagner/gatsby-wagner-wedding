8ee08ee782297f4a970934d518189d8b198b3529
KristopherWagner <kmwagner91@gmail.com>
2022-12-22 12:46:24 -0500

    Use bold text
    This should make the important part stand out better

--- src/components/FAQ/Phones.js
+++ src/components/FAQ/Phones.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Question from './Question';
+
+const Bold = styled.span`
+  font-weight: bold;
+`;
 
 export default function Phones() {
   return (
@@ -12,7 +17,10 @@ export default function Phones() {
         As we celebrate our love and commitment to each other, we ask that our guests take this
         opportunity to be fully present and enjoy the moment with us. In order to create a truly
         special and memorable experience for everyone, we kindly request that guests limit their
-        phone usage during the reception and turn them off during the ceremony. We understand that
+        phone usage during the reception and
+        {' '}
+        <Bold>turn them off during the ceremony</Bold>
+        . We understand that
         it can be tempting to document every moment, so we have hired professionals to photograph,
         record and live stream our events; we hope that you will take this opportunity to be fully
         present and fully engaged in the celebration. Thank you for helping to create a meaningful
